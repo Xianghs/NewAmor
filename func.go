5ba48fd3bd88b43f0bafaee4dad3811e2bc6ed9c
Student15 <student15@macdeMacBook-Pro-XuLiang.local>
2019-09-10 19:12:19 +0800

    func

---
+++ func.go
@@ -0,0 +1,29 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	sum := CalculatePrice(3, 4)
+// 	fmt.Println(sum)
+
+// 	a, _ := Returnfun(10, 20)
+// 	fmt.Println(a)
+
+// 	area, perimeter := rec(2, 4)
+// 	fmt.Println(area, perimeter)
+// }
+
+// func Returnfun(a int, b int) (int, int) {
+// 	return b, a
+// }
+
+// func rec(length, width float64) (area, perimeter float64) {
+// 	area = length * width
+// 	perimeter = (length + width) * 2
+// 	return
+// }
+
+// func CalculatePrice(num int, price int) int {
+// 	sum := num * price
+// 	return sum
+// }
